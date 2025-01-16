dad050450106e9d339386f6fd6309c1294917e92
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2025-01-16 14:15:40 +0530

    Update main.py added cors

--- backend/model/main.py
+++ backend/model/main.py
@@ -19,6 +19,14 @@ import uvicorn
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 load_dotenv()
 
 
